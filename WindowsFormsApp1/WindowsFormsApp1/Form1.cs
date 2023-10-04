24a08db504ac2f2e4b53ee7adb83c046452ff2e2
Thuan17 <thuanhuynh941@gmail.com>
2023-10-04 13:48:57 +0700

    HuynhGiaThuanBuoi07

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Khach> khachhang;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            khachhang = new List<Khach>(); 
+            khachhang.Add(new Khach("thuan"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Khach kh in khachhang)
+            {
+                if (button1.Text == kh.getName())
+                {
+                    label1.Text = kh.getName();
+                }
+            
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Khach.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Khach
+    {
+
+
+        public String Name;
+        public Khach(string name)
+        {
+            this.Name = name;
+
+        }
+        public string getName
+        { get { return this.Name; } }
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
